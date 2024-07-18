09bbebfc5b84b2d5c7faeafbb5af744d440f5e9e
kareena yogi <kareenayogii05@gmail.com>
2024-07-18 18:39:46 +0530

    Add files via upload

---
+++ lecture2.2/kareena2.c
@@ -0,0 +1,13 @@
+//write a program to find the area of a rectangle.
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	float l;
+	
+	printf("enter the length :");
+	scanf("%d",&l);
+	l=l;
+	printf("length of the rectangle : %.3d");
+}
