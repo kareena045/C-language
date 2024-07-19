0f1e3c80c051ab5c498e1a6f588f81c8572aafbd
kareena yogi <kareenayogii05@gmail.com>
2024-07-19 14:06:30 +0530

    Add files via upload

---
+++ lec1.1/kareena1.c
@@ -0,0 +1,12 @@
+//write a program to give a print all arithmetic operator .
+#include<stdio.h>
+#include<conio.h>
+
+ main()
+ {
+ 	printf("+,");
+ 	printf("-,");
+ 	printf("*,");
+ 	printf("/,");
+ 	printf("%");
+ }
