a2283558c9ee387a365ef063258592a47fb97eb3
kareena yogi <kareenayogii05@gmail.com>
2024-07-19 13:56:13 +0530

    Add files via upload

---
+++ lec1.2/kareena1.c
@@ -0,0 +1,10 @@
+//write a program to print your age, name and school.
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	printf("Name : kareena yogi\n");
+	printf("Age : 20\n");
+	printf("school: red and white\n");
+}
---
+++ lec1.2/kareena4.c
@@ -0,0 +1,17 @@
+//write a program to print the given pattern.
+#include<stdio.h>
+#include<conio.h>
+
+ main()
+{
+	printf("*\n");
+	printf("*\n");
+	printf("*\n");
+	printf("*             * *            *\n");
+	printf("*           *    *          * \n");
+	printf("*         *        *      *   \n");
+	printf("*       *            *  *     \n");
+	printf("*     *                       \n");
+	printf("*  *                          \n");
+	printf("*                             \n");
+}
