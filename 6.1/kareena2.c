86f601641252c39c84e9f2993699af91bd3997db
kareena yogi <kareenayogii05@gmail.com>
2024-08-02 18:23:39 +0530

    Add files via upload

---
+++ 6.1/kareena2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int num1;
+	printf("enter your a :");
+	scanf("%d",&num1);
+	
+	int num2;
+	printf("enter your b :");
+	scanf("%d",&num2);
+	
+	(num1>num2)?printf("num1 is maximum !"):printf("num2 is maximum!");
+	
+}
