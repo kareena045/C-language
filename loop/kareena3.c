90bbba48d08c763c2f826574816ba467db011801
kareena yogi <kareenayogii05@gmail.com>
2024-08-30 18:58:31 +0530

    Add files via upload

---
+++ loop/kareena3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int n,i;
+	printf("Enter your n");
+	scanf("%d",&n);
+	
+	while(i<=n)
+	{
+		printf("%d\n",i);
+		i++;
+	}
+
+}
---
+++ loop/kareena6.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int n,i=1;
+	printf("Enter your n");
+	scanf("%d",&n);
+	
+	while(n>=i)
+	{
+		printf("%d\n",n);
+		n--;
+	}
+
+}
