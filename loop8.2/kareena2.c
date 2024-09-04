73c6d60ade82c42ba2f8246fb0c4f912843cc2ba
kareena yogi <kareenayogii05@gmail.com>
2024-09-04 19:05:11 +0530

    Add files via upload

---
+++ loop8.2/kareena2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int i;
+    int n;
+    printf("Enter n :");
+    scanf("%d",&n);
+    
+	for(i=n; i>=1; i--)
+	{
+	     printf("%d ",i);	
+	}
+
+}
