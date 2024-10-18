bb6b9e626020264f8bbdf5287b713b220cdfca00
kareena yogi <kareenayogii05@gmail.com>
2024-10-18 18:32:56 +0530

    Add files via upload

---
+++ 2.2/Q5.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+ main()
+{
+	printf("*\n");
+	printf("*\n");
+	printf("*                        * *\n");
+	printf("*                      *    *\n");
+	printf("*    * *             *\n");
+	printf("*   *    *         *\n");
+	printf("*  *       *     *\n");
+	printf("* *          * *\n");
+	printf("*\n");
+	
+	
+	
+	
+	
+}
---
+++ 2.2/q1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	printf("Name: Kareena yogi\n");
+	printf("Age: 20\n");
+	printf("School:Red & white\n");
+	
+}
